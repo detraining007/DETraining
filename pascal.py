60ad4f3ab3c172d5e98d3428722eb29e7502acce
HarikaSrisailapu <harikasrisailapu@gmail.com>
2025-06-13 10:23:09 +0530

    first commit

---
+++ pascal.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
+    return triangle
+
+num_rows = 6
+triangle = pascal_triangle(num_rows)
+for row in triangle:
+    print(row)
