73c5d5ec684a79d737d57f2fd4fd5f63f32ade38
detraining007 <detraining007@gmail.com>
2025-06-13 12:43:04 +0530

    remove

--- first.py
+++
@@ -1,5 +0,0 @@
-n=int(input("enter the number of rows:"))
-  for i in range (n)
-    for j in range (n
-  
-      
--- list.py
+++
@@ -1,6 +0,0 @@
-num = [2,3,4,5,6]
-sum = 0
-for i in list :
-    sum = sum 
-        print ("the sum is :",sum)
-        
