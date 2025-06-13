64010f122aab4740cb581bd5383fb52d7d4bd9bf
Naga Pavan Kalyan Ganji <123563193+Pawan730@users.noreply.github.com>
2025-06-13 10:47:25 +0530

    Delete generate.py

--- generate.py
+++
@@ -1,9 +0,0 @@
-def generateItem(n):
-    New_List=[]
-    for i in range(n):
-        yield i                   
-
-# for i in generateItem(10):
-#         print(i)
-New_List = list(generateItem(6))
-print(New_List)
