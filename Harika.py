74b42c25913da2ff32fc52da5c47abc846ed480a
HarikaSrisailapu <harikasrisailapu@gmail.com>
2025-06-14 10:17:30 +0530

    hello world

---
+++ Harika.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print("Hello, World!")
+
+# Uncomment the line below to run the hello_world function
+# hello_world()
---
+++ abcdef  pattern.py
@@ -0,0 +1,11 @@
+def iseven (n):
+    return(n % 2 == 0)
+
+
+
+if __name__ + "__main__":
+    n = 56
+    if iseven (n):
+        print("true")
+    else:
+        print("false")
