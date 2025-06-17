b4ae514536da983a4634edc44bd5739743bc8f4d
VamsiPagoti <vamsipagoti234@gmail.com>
2025-06-17 21:36:43 +0530

    push my program

---
+++ inheritance.py
@@ -0,0 +1,20 @@
+class Baseclass():
+    def __init__(self):
+        self.p1=20
+        print("Base class")
+    # def fun(self):
+    #     print("Base class")
+class Derivedclass1(Baseclass):
+    def p2(self):
+        print("Derived class 1")
+class Derivedclass2(Baseclass):
+    def p3(self):
+        print("Derived class 2")
+class childclass(Derivedclass1,Derivedclass2):
+    def p4(self):
+        print("this is child class")
+
+if __name__=="__main__":
+    obj=childclass()
+    obj.p2()
+    # obj1=Baseclass()
