7ec5ffe3f020cd4d48d5a450b5bfe648728a4f3d
AravantiYashwanth <yashwantharavanti@gmail.com>
2025-08-01 19:36:42 +0530

    ll

--- Python/DataStructures/linkedlist.py
+++ Python/DataStructures/linkedlist.py
@@ -1,25 +1,98 @@
-li1 = []
-li2 = []
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
 
-n = int(input("Enter first number: "))
-li1.append(n)
-li1.append(id(n))
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-n = int(input("Enter second number: "))
-li2.append(n)
-li2.append(id(n))
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
+            itr = itr.next
+        print(llstr)
 
-globals_items = list(globals().items())
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count+=1
+            itr = itr.next
 
-print(f"li1: {li1}")  # [value, id]
-print(f"li2: {li2}")  # [value, id]
-print("\nSearching for li1 value (first input):")
+        return count
 
-for name, value in globals_items:
-    if id(value) == li1[1]:
-        print(f"Found in globals: {name} = {value}")
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
 
-print("\nSearching for li2 value (second input):")
-for name, value in globals_items:
-    if id(value) == li2[1]:
-        print(f"Found in globals: {name} = {value}")
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count+=1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert_values(["banana","mango","grapes","orange"])
+    ll.insert_at(1,"blueberry")
+    ll.remove_at(2)
+    ll.print()
+
+    ll.insert_values([45,7,12,567,99])
+    ll.insert_at_end(67)
+    ll.print()
